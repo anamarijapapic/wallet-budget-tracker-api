200af29b65c80ab03d44ede4933d1015142f618c
Anamarija Papić <anamarija.papic@agilo.com>
2025-01-13 18:32:20 +0100

    Refactor user and funds table migrations to use timestamps and add currency support

--- server/db/migrations/20250110181208_create_users_table.js
+++ server/db/migrations/20250110181208_create_users_table.js
@@ -7,7 +7,7 @@ exports.up = function (knex) {
     table.increments('id');
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
-    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamps(true, true);
   });
 };
 
--- server/db/migrations/20250110232533_create_funds_table.js
+++ server/db/migrations/20250110232533_create_funds_table.js
@@ -1,3 +1,5 @@
+const cc = require('currency-codes');
+
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
@@ -6,10 +8,12 @@ exports.up = function (knex) {
   return knex.schema.createTable('funds', (table) => {
     table.increments('id');
     table.string('name').notNullable();
-    table.decimal('balance', 15, 2).notNullable().defaultTo(knex.raw('0.00'));
+    table.enu('currency', cc.codes()).notNullable().defaultTo('EUR');
+    table.decimal('balance', 15, 2).notNullable().defaultTo(0);
     table.integer('user_id').unsigned().notNullable();
+    table.timestamps(true, true);
+
     table.foreign('user_id').references('id').inTable('users');
-    table.timestamp('created_at').defaultTo(knex.fn.now());
   });
 };
 
